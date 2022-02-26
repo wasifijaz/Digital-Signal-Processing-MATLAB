d9e1dccb76815032550d619515616b471a1ab4f6
Muhammad Wasif Ijaz <62423571+wasifijaz@users.noreply.github.com>
2022-02-26 22:57:35 +0500

    Implement Discrete Time Signal (LTI System) Using MATLAB

---
+++ Lab 3/P1T2.m
@@ -0,0 +1,50 @@
+%%%%%Problem 1 Task 2%%%%%
+n=0:30;
+x1_n=cos(2*pi*0.1*n);
+subplot(4,2,1)
+stem(n,x1_n)
+xlabel('Time scale'); 
+ylabel('Amplitude'); 
+title('First input'); 
+x2_n=cos(2*pi*0.4*n);
+subplot(4,2,2)
+stem(n,x2_n)
+xlabel('Time scale');
+ylabel('Amplitude');
+title('Second input'); 
+num=[3 4];
+den=[1 -2 3];
+y1=filter(num,den,x1_n);
+subplot(4,2,3)
+stem(n,y1)
+xlabel('Time scale'); 
+ylabel('Amplitude');
+title('Output for First input');
+y2=filter(num,den,x2_n);
+subplot(4,2,4)
+stem(n,y2)
+xlabel('Time scale');
+ylabel('Amplitide'); 
+title('Output for second input'); 
+a=2;b=-3;
+x3=a*x1_n+b*x2_n;
+subplot(4,2,5)
+stem(n,x3)
+y3=filter(num,den,x3);
+subplot(4,2,6)
+stem(n,x3)
+xlabel('Time scale'); 
+ylabel('Amplitude'); 
+title('System Output for 2*first input-3*second input'); 
+z=a*y1+(-b)*y2;
+subplot(4,2,7)
+stem(n,z)
+xlabel('Time scale'); 
+ylabel('Amplitude'); 
+title('plot for 2*(first output)-3*(second output)'); 
+p=y3-z;
+subplot(4,2,8)
+stem(n,p)
+xlabel('Time scale');
+ylabel('Amplitude'); 
+title('Verification of linearity');
---
+++ Lab 3/P2T2.m
@@ -0,0 +1,29 @@
+%%%%%Problem 2 Task 2%%%%%
+n=0:40;
+a=3;b=6;D=10;
+x1=cos(2*pi*0.6*n);
+x2=cos(2*pi*0.9*n);
+x=a*x1+b*x2;
+xd=[zeros(1,D) x];
+num=[3 5 0 0 10];den=[1];
+y=filter(num,den,x);
+yd=filter(num,den,xd);
+d=y-yd(1+D:41+D);
+subplot(4,1,1)
+stem(n,y)
+ylabel('Amplitude');
+title('Output y[n]');
+subplot(4,1,2)
+stem(n,xd(1:41))
+ylabel('Amplitude');
+title('Delayed Input ');
+subplot(4,1,3)
+stem(n,yd(1:41))
+ylabel('Amplitude');
+title('Output Due to Delayed Input');
+subplot(4,1,4)
+stem(n,d)
+xlabel('Time index n');
+ylabel('Amplitude');
+title('Difference Signal');
+title('Weighted Output');
